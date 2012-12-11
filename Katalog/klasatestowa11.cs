af898b740bcce2e72b866fa30edaab121d570695
krauzaw <krauzaw@gmail.com>
2012-12-11 15:03:30 +0100

    katalog

---
+++ Katalog/klasatestowa11.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katalog
+{
+    class klasatestowa11
+    {
+        string aaaa;
+        public char c;
+    }
+}
